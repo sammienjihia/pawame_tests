2c30c8a67cb23a55e18d7aa079c38a5b71d90af8
sammienjihia <dilaninjihia@gmail.com>
2018-12-18 13:02:19 +0300

    Exposed a random number generator end point

--- pawame/graphapp/urls.py
+++ pawame/graphapp/urls.py
@@ -3,4 +3,5 @@ from graphapp import views
 
 urlpatterns = [
     url(r"^graph$", views.GraphDataView.as_view(), name="graph"),
+    url(r"^generator/$", views.RandomNumberGen.as_view(), name="generator"),
 ]
